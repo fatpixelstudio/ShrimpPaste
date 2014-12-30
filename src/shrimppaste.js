8836f30900a69d8cb26810eddccbf47c13362e0c
Marijn Tijhuis <marijn@studiodumbar.com>
2014-12-30 16:26:58 +0100

    Implement basic slide functionality.

--- src/shrimppaste.js
+++ src/shrimppaste.js
@@ -6,7 +6,9 @@ window.shrimppaste = (function (window, document, undefined) {
 
 	'use strict';
 
-	var shrimppaste = {}
+	var shrimppaste = {
+		steps : {}
+	}
 
 	// Feature Test
 	if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
@@ -16,21 +18,59 @@ window.shrimppaste = (function (window, document, undefined) {
 		console.log('your browser is kind of old. Lets serve an alternate version');
 	}
 
+	shrimppaste.createNav = function(type, parent) {
+		var button = document.createElement('a');
+		button.setAttribute('href', '#');
+		button.classList.add('ShrimpPaste-button');
+		button.classList.add('ShrimpPaste-button--'+type);
+		button.classList.add('js-'+type);
+		button.textContent = type;
+		return button;
+	}
+
+	shrimppaste.handleSlideClick = function(event, direction, parent) {
+		event.preventDefault();
+		if(direction === 'prev') {
+			shrimppaste.steps.current = shrimppaste.steps.current + shrimppaste.steps.width;
+		}
+		else {
+			shrimppaste.steps.current = shrimppaste.steps.current - shrimppaste.steps.width;
+		}
+
+		var slidesWrap = parent.querySelector('.js-shrimppaste-wrap');
+		slidesWrap.style.transform = 'translateX('+shrimppaste.steps.current+'px)';
+	}
+
 	shrimppaste.init = function (opts) {
 		var sliders = document.querySelectorAll('.js-shrimppaste');
-		for(var i = 0; i <= sliders.length; i++) {
+		for(var i = 0; i < sliders.length; i++) {
 			shrimppaste.build(sliders[i], opts);
 		}
 	}
 
 	shrimppaste.build = function (sliderEl, opts) {
-		var maxWidth = sliderEl.offsetWidth;
+		var slidesViewport = sliderEl.querySelector('.js-shrimppaste-viewport');
+		var maxWidth = slidesViewport.offsetWidth;
 		var slideWidth = maxWidth / opts.slides;
-		console.log(maxWidth, slideWidth);
-
 		var slides = sliderEl.querySelectorAll('.js-shrimppaste-item');
-		for(var i = 0; i <= slides.length; i++) {
-			console.log(slides[i]);
+		var totWidth = slideWidth * slides.length;
+		var slidesWrap = sliderEl.querySelector('.js-shrimppaste-wrap');
+
+		slidesWrap.style.width = totWidth.toString() + 'px';
+		sliderEl.classList.add('is-active');
+		shrimppaste.steps.width = slideWidth;
+		shrimppaste.steps.current = 0;
+
+		var prevButton = shrimppaste.createNav('prev',sliderEl);
+		var nextButton = shrimppaste.createNav('next',sliderEl);
+		sliderEl.appendChild(prevButton);
+		sliderEl.appendChild(nextButton);
+
+		nextButton.addEventListener('click', function(event){ shrimppaste.handleSlideClick(event, 'next', sliderEl); }, false);
+		prevButton.addEventListener('click', function(event){ shrimppaste.handleSlideClick(event, 'prev', sliderEl); }, false);
+
+		for(var i = 0; i < slides.length; i++) {
+			slides[i].style.width = slideWidth.toString() +'px';
 		}
 	}
 
