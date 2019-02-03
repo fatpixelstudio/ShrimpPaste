908a435cd06a35c79e2ac941a62b01a9d971ff0c
Marijn Tijhuis <post@fatpixel.nl>
2019-02-03 13:13:54 +0100

    Add basic ShripPaste Lightbox.
    Still some stuff to finish.

--- src/shrimppaste.js
+++ src/shrimppaste.js
@@ -1,141 +1,109 @@
 /**
- * Shrimp Paste
- * ------------
- * This is the plain javascript variant, if you are using jQuery and want to
- * support older browsers, use the jquery variant!
+ * ShrimpPaste minimalist Javascript lightbox
  */
 
- function shrimppasteclass() {
+var ShrimpPaste = (function () {
+	var elements = {
+		backdrop: null,
+		openmodal: null
+	};
 
-	'use strict';
+	function whichTransitionEvent() {
+		var t;
+		var el = document.createElement('fakeelement');
+		var transitions = {
+			'transition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'MozTransition': 'transitionend',
+			'WebkitTransition': 'webkitTransitionEnd'
+		};
 
-	var self = this;
-
-	this.steps = {}
-
-	this.createNav = function(type, parent) {
-		var button = document.createElement('a');
-		button.setAttribute('href', '#');
-		button.classList.add('ShrimpPaste-button');
-		button.classList.add('ShrimpPaste-button--'+type);
-		button.classList.add('js-'+type);
-		button.textContent = type;
-		return button;
+		for (t in transitions) {
+			if (el.style[t] !== undefined) {
+				return transitions[t];
+			}
+		}
 	}
 
-	this.setAllNavActive = function(sliderEl) {
-		sliderEl.querySelector('.js-next').classList.remove('ShrimpPaste-button--inActive');
-		sliderEl.querySelector('.js-prev').classList.remove('ShrimpPaste-button--inActive');
-	}
+	var transitionEnd = whichTransitionEvent();
 
-	this.setNavInactive =  function(event) {
-		var button = event.target;
-		button.classList.add('ShrimpPaste-button--inActive');
-	}
-
-	this.handleSlideClick = function(event, direction, parent) {
+	function closeModal(event) {
 		event.preventDefault();
 
-		self.setAllNavActive(parent);
+		if (elements.openmodal !== null) {
+			elements.backdrop.addEventListener(transitionEnd, function endTransitionBackdropClose() {
+				elements.backdrop.classList.remove('is-visible');
+				elements.backdrop.classList.remove('is-hiding');
+				this.removeEventListener(transitionEnd, endTransitionBackdropClose, false);
+			}, false);
 
-		if(direction === 'prev') {
-			// Check if current step is possible
-			if((self.steps.current + self.steps.width) <= 0){
-				self.steps.current = self.steps.current + self.steps.width;
+			elements.backdrop.classList.add('is-hiding');
+		}
 
-				// Check if *next* step is even possible
-				if((self.steps.current + self.steps.width) > 0){
-					self.setNavInactive(event);
+		if (elements.openmodal !== null) {
+			elements.openmodal.addEventListener(transitionEnd, function endTransitionBackdropClose() {
+				elements.openmodal.classList.remove('is-visible');
+				elements.openmodal.classList.remove('is-hiding');
+				this.removeEventListener(transitionEnd, endTransitionBackdropClose, false);
+			}, false);
+
+			elements.openmodal.classList.add('is-hiding');
+		}
+	}
+
+	function addBackdropHandlers() {
+		elements.backdrop.addEventListener('click', closeModal, false);
+	}
+
+	function addModalHandlers(modal) {
+		var closebutton = modal.querySelector('.js-shrimppaste-close');
+		closebutton.addEventListener('click', closeModal, false);
+	}
+
+	function showModal(event) {
+		event.preventDefault();
+
+		elements.backdrop.classList.add('is-visible');
+		addBackdropHandlers();
+
+		var link = event.target;
+		if (link.classList.contains('js-shrimppaste-open') === false) {
+			link = domparents.findAncestorByClass(link, 'js-shrimppaste-open');
+		}
+		var id = link.getAttribute('href').substr(1);
+		elements.openmodal = document.getElementById(id);
+
+		if (elements.openmodal !== null) {
+			elements.openmodal.classList.add('is-visible');
+			addModalHandlers(elements.openmodal);
+		}
+	}
+
+	function addBackdrop() {
+		elements.backdrop = document.createElement('div');
+		elements.backdrop.classList.add('shrimppaste-backdrop', 'js-shrimppaste-backdrop');
+		document.body.appendChild(elements.backdrop);
+	}
+
+	function init(opts) {
+		var modals = document.querySelectorAll('.js-shrimppaste');
+		var modalbuttons = document.querySelectorAll('.js-shrimppaste-open');
+
+		if (modals !== null && modals.length !== 0) {
+			addBackdrop();
+
+			if (modalbuttons !== null && modalbuttons.length !== 0) {
+				for (i = 0; i < modalbuttons.length; i++) {
+					modalbuttons[i].addEventListener('click', showModal, false);
 				}
 			}
-			else {
-				// Current step not possible, keep button inactive
-				self.setNavInactive(event);
-			}
-		}
-		else {
-			// Check if current step is possible
-			if(self.steps.current - self.steps.width >= -1 * self.steps.max){
-				self.steps.current = self.steps.current - self.steps.width;
-
-				// Check if *next* step is even possible
-				if(self.steps.current - self.steps.width <= -1 * self.steps.max){
-					self.setNavInactive(event);
-				}
-			}
-			else {
-				// Current step not possible, keep button inactive
-				self.setNavInactive(event);
-			}
-		}
-
-		var slidesWrap = parent.querySelector('.js-shrimppaste-wrap');
-		slidesWrap.style.transform = 'translateX('+self.steps.current+'px)';
-	}
-
-	this.build = function (sliderEl, opts) {
-		var slidesViewport = sliderEl.querySelector('.js-shrimppaste-viewport');
-		var maxWidth = Math.round(slidesViewport.offsetWidth);
-		var slideWidth = Math.round(maxWidth / opts.slides);
-		var slides = sliderEl.querySelectorAll('.js-shrimppaste-item');
-		var totWidth = Math.round(slideWidth * slides.length);
-		var slidesWrap = sliderEl.querySelector('.js-shrimppaste-wrap');
-
-		slidesWrap.style.width = totWidth.toString() + 'px';
-		sliderEl.classList.add('is-active');
-		self.steps.width = slideWidth;
-		self.steps.max = totWidth - maxWidth;
-		self.steps.current = 0;
-
-		if(self.steps.max > slideWidth) {
-			var prevButton = self.createNav('prev',sliderEl);
-			var nextButton = self.createNav('next',sliderEl);
-			sliderEl.appendChild(prevButton);
-			sliderEl.appendChild(nextButton);
-
-			prevButton.classList.add('ShrimpPaste-button--inActive');
-
-			nextButton.addEventListener('click', function(event){ self.handleSlideClick(event, 'next', sliderEl); }, false);
-			prevButton.addEventListener('click', function(event){ self.handleSlideClick(event, 'prev', sliderEl); }, false);
-		}
-
-		for(var i = 0; i < slides.length; i++) {
-			slides[i].style.width = slideWidth.toString() +'px';
 		}
 	}
 
-};
-
-window.shrimppaste = (function (window, document, undefined) {
-
-	'use strict';
-
-	var shrimppaste = {
-		steps : {}
-	}
-
-	// Feature Test
-
-	shrimppaste.cutsMustard = function() {
-		if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-			return true;
-		}
-		else {
-			// If feature test returns false: you might consider using the jQuery variant
-			return false;
-		}
-	}
-
-	shrimppaste.init = function (opts) {
-		if(shrimppaste.cutsMustard()) {
-			var sliders = document.querySelectorAll('.js-shrimppaste');
-			for(var i = 0; i < sliders.length; i++) {
-				var shrimppasteinstance = new shrimppasteclass();
-				shrimppasteinstance.build(sliders[i], opts);
-			}
-		}
-	}
-
-	return shrimppaste;
-
-})(window, document);
+	/**
+	 * Return public methods
+	 */
+	return {
+		init: init
+	};
+})();
